cd8f871cbc90fa38d52c83f903d82c7d7fbc92e7
luke <lukepratt@Lukes-MacBook-Air.local>
2023-12-09 17:43:00 -0900

    fix

--- books/urls.py
+++ books/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import forms
 from . import views
@@ -24,5 +24,5 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(success_url= reverse_lazy('login-page')), name='password_reset_complete'),
 ]
