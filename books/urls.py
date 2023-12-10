202327f4dc4d2530fbede2e795531efc7d6c2f14
luke <lukepratt@Lukes-MacBook-Air.local>
2023-12-09 17:45:35 -0900

    fixed

--- books/urls.py
+++ books/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(success_url= reverse_lazy('login-page')), name='password_reset_complete'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
